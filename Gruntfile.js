1dc8d88d2dacee6a0ab3e14a43539426a0797e15
XhmikosR <xhmikosr@gmail.com>
2015-04-23 13:17:45 +0300

    Grunt: extend `prep-release` task.
    Make it so that everything is done for us to reduce the possibility of missing something when releasing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -513,9 +513,26 @@ module.exports = function (grunt) {
   grunt.registerTask('lint-docs-css', ['csslint:docs', 'csslint:examples']);
   grunt.registerTask('docs-js', ['uglify:docsJs', 'uglify:customize']);
   grunt.registerTask('lint-docs-js', ['jshint:assets', 'jscs:assets']);
-  grunt.registerTask('docs', ['docs-css', 'lint-docs-css', 'docs-js', 'lint-docs-js', 'clean:docs', 'copy:docs', 'build-glyphicons-data', 'build-customizer']);
+  grunt.registerTask('docs', [
+    'docs-css',
+    'lint-docs-css',
+    'docs-js',
+    'lint-docs-js',
+    'clean:docs',
+    'copy:docs',
+    'build-glyphicons-data',
+    'build-customizer'
+  ]);
+  grunt.registerTask('docs-build', [
+    'docs-css',
+    'docs-js',
+    'clean:docs',
+    'copy:docs',
+    'build-glyphicons-data',
+    'build-customizer'
+  ]);
 
-  grunt.registerTask('prep-release', ['jekyll:github', 'htmlmin', 'compress']);
+  grunt.registerTask('prep-release', ['dist', 'docs-build', 'jekyll:github', 'htmlmin', 'compress']);
 
   // Task for updating the cached npm packages used by the Travis build (which are controlled by test-infra/npm-shrinkwrap.json).
   // This task should be run and the updated file should be committed whenever Bootstrap's dependencies change.
